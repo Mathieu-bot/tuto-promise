034ec7ebb67c4172eb66bbf5b2e0233b784a18bf
Mathieu <hei.tafita.2@gmail.com>
2025-08-06 05:14:48 +0300

    fix: speed-insight importation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,17 +4,20 @@ import { Toaster } from 'react-hot-toast';
 import CharacterManager from './components/CharacterManager';
 import PWAInstallPrompt from './components/PWAInstallPrompt';
 import { theme } from './theme/theme';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <CharacterManager />
-      <PWAInstallPrompt />
-      <Toaster />
-    </ThemeProvider>
-     <SpeedInsights />  );
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <CharacterManager />
+        <PWAInstallPrompt />
+        <Toaster />
+      </ThemeProvider>
+      <SpeedInsights />
+    </>
+  );
 }
 
 export default App;
